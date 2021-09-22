dcdc8f35935217bae0be8ecfa82fb9ff565c7110
Kaleb-Aguirre03 <820930@mystma.org>
2021-09-22 09:50:33 -0500

    #1

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-#WRITE YOUR CODE IN THIS FILE
+#Define the function sumDiff
+def sumDiff(a, b):
+
+    #return value 
+    return (a + b) - (a - b)
+
+#print the funtion in order to get 10
+print (sumDiff (10, 5))
+
+#print the funtion in order to get 4
+print (sumDiff (17, 2))
+
+#print the funtion in order to get 198
+print (sumDiff (123, 99))
